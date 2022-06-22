9973e86b8fb8a9e1f8ce8c47653b3cfab6005f28
F-mm1229 <meru112mofmof129@gmail.com>
2022-06-22 23:00:12 +0900

    work10-4

--- week10/work10-4.c
+++ week10/work10-4.c
@@ -1,7 +1,6 @@
-// 必須課題 10-3
+// 必須課題 10-4
 
 #include <stdio.h>
-#define HEAP_SIZE 20
 
 /* 値を交換する関数 */
 void swap(int *x, int *y) {
@@ -9,20 +8,6 @@ void swap(int *x, int *y) {
     temp = *x;
     *x = *y;
     *y = temp;
-}
-
-/* ヒープに新たなデータを挿入する関数 */
-void insert(int val, int a[], int *n) {
-    *n += 1;
-    int num = *n;
-    a[num] = val;
-    while(1) {
-        if(num > 1 && a[num] >= a[num / 2]) {
-            break;
-        }
-        swap(&a[num], &a[num / 2]);
-        num = num / 2;
-    }
 }
 
 /* ヒープ条件を満たしているか確かめる関数 */
@@ -43,12 +28,42 @@ int check_heap(int a[], int n) {
     return 1;
 }
 
+/* ヒープの中から最小値を取り除き返り値として返す関数 */
+int deletmin(int a[], int *n) {
+    swap(&a[1], &a[*n]);
+    int delete_num = a[*n];
+    a[*n] = '\0';
+    *n -= 1;
+    int num = *n;
+    
+    int i = 1;
+    
+    while(i < *n) {
+        if(2*i <= *n && a[2*i] <= a[2*i+1]) {
+            if(a[i] <= a[2*i]) {
+                break;
+            }
+            swap(&a[i], &a[2*i]);
+            i = 2*i;
+        } else if (2*i+1 <= *n && a[2*i] > a[2*i+1]) {
+            if(a[i] <= a[2*i+1]) {
+                break;
+            }
+            swap(&a[i], &a[2*i+1]);
+            i = 2*i+1;
+        } else {
+            break;
+        }
+    }
+    return delete_num;
+}
+
 int main(void)
 {
-    int heap[HEAP_SIZE + 1] = {'\0', 4, 7, 18, 10, 15, 32, 20, 38, 13, 29};
-    int heap_size = 10;
+    int heap[12] = {'\0', 4, 5, 18, 10, 7, 32, 20, 38, 13, 29, 15};
+    int heap_size = 11;
 
-    insert(5, heap, &heap_size);
+    deletmin(heap, &heap_size);
 
     if (check_heap(heap, heap_size) == 0) {
         printf("ヒープ条件を満たしていません。\n");
